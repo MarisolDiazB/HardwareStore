ba51ce333422f73aa275714e52f12fee63f07284
Marisol Diaz <marisoldiaz287872@correo.itm.edu.co>
2024-05-15 00:24:45 -0500

    Test994

---
+++ HardwareStore/Data/Entities/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HardwareStore.Data.Entities
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
