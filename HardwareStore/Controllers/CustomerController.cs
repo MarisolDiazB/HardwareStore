7314d15ed8a33b257a9c41aaff059a85997b1d8f
iHamiltonRios <Kalimbar23@gmail.com>
2024-05-22 22:52:20 -0500

    no message

--- HardwareStore/Controllers/CustomerController.cs
+++ HardwareStore/Controllers/CustomerController.cs
@@ -1,8 +1,8 @@
-﻿using AspNetCoreHero.ToastNotification.Abstractions;
-using HardwareStore.Core;
+﻿using AspNetCoreHero.ToastNotification.Abstractions; // importa la interfaz para el servicio de notificaciones.
+using HardwareStore.Core.Pagination;
 using HardwareStore.Data.Entities;
 using HardwareStore.Services;
-using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc; // importa el espacio de nombres de ASP.NET Core para MVC.
 
 namespace HardwareStore.Controllers
 {
@@ -10,41 +10,118 @@ namespace HardwareStore.Controllers
     {
         private readonly ICustomerService _services;
         private readonly INotyfService _notify;
+        private const int PageSize = 10;
 
-        public CustomerController(ICustomerService productServices, INotyfService notify)
+        // constructor de la clase CustomerController.
+        public CustomerController(ICustomerService customerService, INotyfService notify)
         {
-            _services = productServices;
+            _services = customerService; // Inicializa
             _notify = notify;
         }
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
-            Response<List<Customer>> list = await _services.GetListAsync();
 
-            if (!list.IsSuccess)
+        // mostrar la lista de clientes.
+        [HttpGet]
+        public async Task<IActionResult> Index(int pageNumber = 1)
+        {
+            //  solicitud de paginación.
+            var paginationRequest = new PaginationRequest
             {
-                return RedirectToAction("Index", "Home");
-            }
-            return View(list.Result);
+                PageNumber = pageNumber,
+                RecordsPerPage = PageSize
+            };
+
+            // obtiene la lista de clientes de forma asincrónica.
+            var response = await _services.GetListAsync(paginationRequest);
+            var customers = response.Result;
+            var totalItems = response.Result.Count;
+
+            // el ViewModel para la paginación.
+            var viewModel = new PaginacionViewModel
+            {
+                Customers = customers,
+                PaginaActual = pageNumber,
+                TotalPaginas = (int)Math.Ceiling(totalItems / (double)PageSize)
+            };
+
+
+            return View(viewModel);
         }
 
+        // para mostrar el formulario de creación de cliente.
         [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
+        // para procesar 
         [HttpPost]
         public async Task<IActionResult> Create(Customer model)
         {
             try
             {
+                // Valida el modelo.
                 if (!ModelState.IsValid)
                 {
                     _notify.Error("Debe ajustar los errores de validación.");
                     return View(model);
                 }
-                Response<Customer> response = await _services.CreateAsync(model);
+
+                // Crea el cliente de forma asincrónica.
+                var response = await _services.CreateAsync(model);
+
+                // Maneja la respuesta del servicio.
+                if (response.IsSuccess)
+                {
+                    _notify.Success(response.Message);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                _notify.Error(response.Message);
+                return View(model);
+            }
+            catch (Exception ex)
+            {
+                _notify.Error(ex.Message);
+                return View(model);
+            }
+        }
+
+        //  para mostrar el formulario de edición de cliente.
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var response = await _services.GetOneAsync(id);
+
+            if (response.IsSuccess)
+            {
+                var customer = response.Result;
+                return View(customer);
+            }
+            else
+            {
+                _notify.Error(response.Message);
+                return RedirectToAction(nameof(Index));
+            }
+        }
+
+        // para procesar 
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, Customer model)
+        {
+            try
+            {
+                // valida el modelo.
+                if (!ModelState.IsValid)
+                {
+                    _notify.Error("Debe ajustar los errores de validación.");
+                    return View(model);
+                }
+
+                model.Id = id;
+
+                // edita el cliente de forma asincrónica.
+                var response = await _services.EditAsync(model);
 
                 if (response.IsSuccess)
                 {
@@ -62,6 +139,31 @@ namespace HardwareStore.Controllers
             }
         }
 
+        // para eliminar un cliente.
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            try
+            {
+                // elimina el cliente de forma asincrónica.
+                var response = await _services.DeleteAsync(id);
+
+
+                if (response.IsSuccess)
+                {
+                    _notify.Success(response.Message);
+                }
+                else
+                {
+                    _notify.Error(response.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                _notify.Error("Se produjo un error al intentar eliminar el cliente: " + ex.Message);
+            }
+
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
-
