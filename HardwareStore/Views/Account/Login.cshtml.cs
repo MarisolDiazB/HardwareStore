7bd0cc833889ba4d31d2be7feec5cb64a03a81f9
alexxch7 <alexanderchavarria290244@correo.itm.edu.co>
2024-05-24 00:02:16 -0500

    Login y Register
    view, entities, models relacionados a login y register (todavía en prueba ya que hay errores)

---
+++ HardwareStore/Views/Account/Login.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HardwareStore.Views.Account
+{
+    public class LoginModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
